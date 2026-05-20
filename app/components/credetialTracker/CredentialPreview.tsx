f46affba8aa4b343953ae4b8214166e5cadebb8f
Joshua R. <avgn777@gmail.com>
2026-05-20 08:36:52 +0000

    Adding OCR to Suggested, with remove suggestions to reduce visual clutter

--- app/components/credetialTracker/CredentialPreview.tsx
+++ app/components/credetialTracker/CredentialPreview.tsx
@@ -1066,6 +1066,16 @@ const CredentialPreview: React.FC<CredentialPreviewProps> = ({
                     </Box>
                   </Box>
                 ))}
+                
+                <Box sx={{ display: 'flex', gap: 0, flexWrap: 'wrap', mt: 0, mb:0 }}> 
+                  <Button onClick={() => {
+                    setAcceptedSuggestedSkills(prev => {
+                      const next = new Set(prev)
+                      suggestedSkills.forEach(skill => next.add(skill.name.toLowerCase()))
+                      return next
+                    })
+                  }}> Remove Suggested Skills </Button>
+                </Box>
               </Box>
             )}
           </Box>
@@ -1101,8 +1111,11 @@ const CredentialPreview: React.FC<CredentialPreviewProps> = ({
               </Box>
             ))}
           </Box>
-          <hr/>
-          <Button onClick={handlePermanentlyRemove}> Permanently Remove </Button>
+          
+          <Box sx={{ display: 'flex', gap: 1, flexWrap: 'wrap', mt: 2 }}> 
+            <Button onClick={handlePermanentlyRemove}> Permanently Remove Skills </Button>
+          </Box>
+          
         </Box>
       )}
 
